3a35b9419f57a775b73db9d83ca789787afc78b8
shunsuke-oka <71703108+shunsuke-oka@users.noreply.github.com>
2020-10-23 17:09:39 +0900

    新規登録のバリデーション、エラー文の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  
+  year_month_day = /\A\d{4}-\d{2}-\d{2}\z/
+  validates :birthday, presence: true, format: { with: year_month_day }
 end
