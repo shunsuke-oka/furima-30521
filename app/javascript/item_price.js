f708f105921a518053ae65e1d82322c906ce3346
shunsuke-oka <71703108+shunsuke-oka@users.noreply.github.com>
2020-10-28 20:09:15 +0900

    出品機能で手数料処理のjavascriptの実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    const tax = inputValue * 0.1;
+    const price_tax = Math.floor(tax);
+    addTaxDom.innerHTML = `${price_tax}`
+
+    const profit = document.getElementById("profit");
+    const gain = inputValue - price_tax;
+    profit.innerHTML = `${gain}`
+  })
+});
