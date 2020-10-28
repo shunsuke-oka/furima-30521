bd2d3111706cb1b3d76d00d1e4c849e01dd944d2
shunsuke-oka <71703108+shunsuke-oka@users.noreply.github.com>
2020-10-28 14:10:26 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
-    
   end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.new(item_params)
+    if @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  
+
+
 
   private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,32 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :price
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
+
+  with_options presence: true do
+    validates :category
+    validates :status
+    validates :delivery
+    validates :area
+    validates :day
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_id
+    validates :area_id
+    validates :day_id
+  end
 end
